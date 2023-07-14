a0ba1638afd5134de44442fb5a6393be7521b400
Khensanincube <khensanincube@ymail.com>
2023-07-14 06:00:08 -0700

    prints lowercase alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - to print lowercase alphabets except q and e
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
