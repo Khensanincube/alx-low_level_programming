4201aba057a581359a5e6f318cc2fb39369f6acf
Khensanincube <khensanincube@ymail.com>
2023-07-18 07:04:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,17 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - prints putchar
+ * main - Entry point
  *
- * Return:  always 0.
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	/*prints putchar on the screen*/
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0); /* returns 0 */
+
+printf("_putchar\n");
+
+return (0);
 }
+
